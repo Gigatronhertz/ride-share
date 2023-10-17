b60a38c567e3a5f9b805420635fa7c393fa0eb2a
Gigatronhertz <99260886+Gigatronhertz@users.noreply.github.com>
2023-10-17 19:28:33 +0100

    done with first screen

---
+++ lib/components/colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+const Color green =  Color.fromARGB(255, 8, 183, 131);
--- lib/screens/SplashScreen.dart
+++ lib/screens/SplashScreen.dart
@@ -1,11 +1,23 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:rideshare/components/colors.dart';
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
 
-
+class _SplashScreenState extends State<SplashScreen> {
+@override
+void initState(){
+  super.initState();
+  Future.delayed(Duration(seconds: 3),(){
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) =>OnboardingScreen()));
+  });
+}
 
   @override
   Widget build(BuildContext context) {
